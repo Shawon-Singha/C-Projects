f0189fe66c7833e63595458ea046ef4f7725270f
Your Name <shawonsingha10@gmail.com>
2023-03-31 23:59:32 +0600

    update

--- C/Snake_Game/Snake.c
+++ C/Snake_Game/Snake.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 
-#define m 30
+#define m 40
+#define n 20
 
 int i,j;
 
@@ -16,6 +17,44 @@ void main()
       else if(i == m+1)
       {
          printf("%c",187);
+      }
+
+      else
+      {
+         printf("%c",205);
+      }
+   }
+
+   printf("\n");
+
+   for(i = 0;i<n;i++) //number of row
+   {
+      printf("%c",186);
+
+      for(j = 0; j<m;j++) //number of coloum
+      {
+         if( j == m-1)
+         {
+            printf(" %c\n",186);
+         }
+         else
+         {
+            printf(" ");
+         }
+      }
+
+   }
+
+   for(i = 0;i<=m+1;i++)
+   {
+      if(i== 0)
+      {
+         printf("%c",200);
+      }
+
+      else if(i == m+1)
+      {
+         printf("%c",188);
       }
 
       else
